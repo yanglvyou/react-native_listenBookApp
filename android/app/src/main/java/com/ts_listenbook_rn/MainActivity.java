0cdfd0ba1b4fbccb9c1d645b9188c914586bcd53
lvyou <376607092@qq.com>
2020-05-02 22:09:10 +0800

    初始化

---
+++ android/app/src/main/java/com/ts_listenbook_rn/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ts_listenbook_rn;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "TS_ListenBook_RN";
+  }
+}
