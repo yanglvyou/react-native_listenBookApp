9266c54ad116be3dd53f84555a3fb6a039c68ad5
lvyou <376607092@qq.com>
2020-05-04 13:10:50 +0800

    配置react-navigation 5.x

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaView, View, Text} from 'react-native';
 import {url} from '@/utils/index';
 
 export default function Index() {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>少时诵诗书所</Text>
-        <Text>{url}</Text>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaView>
+        <View>
+          <Text>少时诵诗书所33333113444444</Text>
+          <Text>{url}</Text>
+        </View>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 }
